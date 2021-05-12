409da33510f158faf64a8ac3025348842a077384
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-05-13 00:49:05 +0100

    the get method

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const url = "mongodb://localhost:herokuDB";
+
+const port = process.env.PORT || 56000;
+const app = express();
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+  console.log("successfully deployed");
+});
+
+app.get("/", (req, res) => {
+  res.send("hosting this api on heroku now ..!");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
